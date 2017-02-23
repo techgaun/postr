2bc05f13601d908d700f6ae5d3b2c6b03b1a4700
techgaun <coolsamar207@gmail.com>
2017-02-23 11:55:14 -0600

    initial updates

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,7 @@
 defmodule Postr.PageController do
   use Postr.Web, :controller
 
-  def index(conn, _params) do
+  def index(conn, params) do
     render conn, "index.html"
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Postr.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    post "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
